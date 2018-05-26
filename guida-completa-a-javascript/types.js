848d94cd51c2dc1b40b7cf127de3919d2d36f741
Andrea Sonny <andreasonny83@gmail.com>
2018-05-26 18:45:26 +0100

    Guida completa a JavaScript #2

---
+++ guida-completa-a-javascript/types.js
@@ -0,0 +1,16 @@
+var piGreco;
+
+piGreco = 3.14;
+
+console.log('piGreco', piGreco);
+console.log('type', typeof piGreco);
+
+piGreco = '3.14';
+
+console.log('piGreco', piGreco);
+console.log('type', typeof piGreco);
+
+piGreco = true;
+
+console.log('piGreco', piGreco);
+console.log('type', typeof piGreco);
