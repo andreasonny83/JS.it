0a3f4096b927ba7f3c779c9153df41c76f1047b3
Andrea Sonny <andreasonny83@gmail.com>
2018-07-21 18:07:01 +0100

    Funzioni in JavaScript

---
+++ guida-completa-a-javascript/this.js
@@ -0,0 +1,13 @@
+gattiDeclaration('Penny');
+
+// Function declaration
+function gattiDeclaration(gatto) {
+  console.log(gatto);
+}
+
+// Function expression
+var gattiExpression = function(gatto) {
+  console.log(gatto);
+}
+
+gattiExpression('Woody');
